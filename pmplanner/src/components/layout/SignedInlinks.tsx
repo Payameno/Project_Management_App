b693468ac9db44fd678783ff692b0875b431cd7a
Payam Beigi <beigi.payam@gmail.com>
2023-01-22 17:28:28 -0800

    Create components, add router

---
+++ pmplanner/src/components/layout/SignedInlinks.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SignedInlinks = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignedInlinks
---
+++ pmplanner/src/components/project/ProjectSummary.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProjectSummary = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default ProjectSummary
